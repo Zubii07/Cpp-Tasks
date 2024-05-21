1c5d9cd3b4c3c1fa31ed78029ec12ccdaeef561b
M Zohaib <167600779+Zubii07@users.noreply.github.com>
2024-05-22 01:51:03 +0500

    Add files via upload
    Find a number using Array

---
+++ find a num.cpp
@@ -0,0 +1,29 @@
+  #include<iostream>
+using namespace std;
+main(){
+	int n;
+	cout<<" enter the size of array:";
+	cin>>n;
+	int arr[n];
+	for(int i=0;i<n;i++){
+		cout<<" enter the value of "<<i+1<<"th member: "<<endl;
+		cin>>arr[i];
+	}
+	int n2;
+	cout<<" Enter the num you want to find in an array: "<<endl;
+	cin>>n2;
+	int found=0;
+	
+	 for(int i=0;i<n;i++){
+	 	if(i==n2){
+	 		cout<<" the num is located in the array at the position "<<i<<endl;
+	 		 found=1;
+		 }
+	 }	
+	 if(found==0){
+	 	cout<<" the num is not in array"<<endl;
+	 }
+		
+	}
+
+
