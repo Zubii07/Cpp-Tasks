d18ccf1a626a866d55fe710017fd5cb692963d58
M Zohaib <167600779+Zubii07@users.noreply.github.com>
2024-05-22 01:50:13 +0500

    Add files via upload
    Make Matrix using 2D Array

---
+++ 2Darray.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include<climits>
+using namespace std;
+main(){
+   int i,j;
+   cout<<" Enter i: "<<endl;
+   cin>>i;
+   cout<<" Enter j: "<<endl;
+   cin>>j;
+   int arr[i][j];
+   for(int n=0;n<i;n++){
+    for(int m=0;m<j;m++){
+        cin>>arr[n][m];
+    }
+   }
+   cout<<" matrix is  "<<endl;
+
+for(int n=0;n<i;n++){
+    for(int m=0;m<j;m++){
+        cout<<arr[n][m]<<" ";
+    }
+    cout<<endl;
+   }
+
+
+    return 0;
+}
